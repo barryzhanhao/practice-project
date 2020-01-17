ae192ab7166b0ec25260eddacabd6eb4ed9fa898
barryzhanhao <travelinglight365@gmail.com>
2020-01-17 11:31:47 +0800

    fix bugs

--- akka/src/test/java/pers/james/practice/akka/ToFindRemoteActorTest.java
+++ akka/src/test/java/pers/james/practice/akka/ToFindRemoteActorTest.java
@@ -5,20 +5,19 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import java.util.concurrent.TimeUnit;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class ToFindRemoteActorTest {
 
     @Test
     public void test() throws InterruptedException {
-        Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port=2551")
-            .withFallback(ConfigFactory.load("application.conf"));
+        Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port=2551");
 
         ActorSystem system = ActorSystem.create("sys", config);
         ActorRef toFind = system.actorOf(Props.create(ToFindRemoteActor.class), "toFind");
 
-        TimeUnit.SECONDS.sleep(5);
+        Assertions.assertThat(toFind).isNotNull();
     }
 
 }
