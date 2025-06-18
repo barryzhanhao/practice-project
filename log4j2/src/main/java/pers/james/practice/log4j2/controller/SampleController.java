1167a927d413d0b81c835e29fe1153b012c7624a
barryzhanhao <travelinglight365@gmail.com>
2025-06-18 09:59:13 +0800

    add log4j2

---
+++ log4j2/src/main/java/pers/james/practice/log4j2/controller/SampleController.java
@@ -0,0 +1,16 @@
+package pers.james.practice.log4j2.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String get() {
+        log.info("hello");
+        return "2222";
+    }
+}
